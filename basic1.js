d32be35fff2051aeb62766338d210fcda3edc37b
Agalya <157816927+agalya-madhesh@users.noreply.github.com>
2024-02-19 15:19:03 +0530

    Add files via upload

---
+++ basic1.js
@@ -0,0 +1,45 @@
+//if else using logical operator
+var score=40
+
+if(score<50)
+{
+    console.log("You need to improve")
+}
+
+if(score>50 && score<=60)
+{
+    console.log("Average")
+}
+
+if(score>80)
+{
+    console.log("Excellent")
+}
+
+//for loop
+for(count=1;count<=10;count=count+1)
+{
+    console.log(count)
+}
+
+for(count=1;count<=10;count=count+2)
+{
+    console.log(count)
+}
+
+for(count=1;count<=10;count=count+2)
+{
+    console.log(count + "x2")
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ basics.js
@@ -0,0 +1,113 @@
+var price=100
+//iphone 15
+var product="iphone"
+var tax=100
+console.log(product)
+var total=price + tax
+console.log(total)
+
+
+//operators-post increment
+var a=10
+var b=a++
+console.log(b)
+console.log(a)
+
+//operators-pre increment
+var a=10
+var b=++a
+console.log(b)
+console.log(a)
+
+//pre decrement 
+var a=30
+var b=--a
+console.log(b)
+console.log(a)
+
+//pre decrement 
+var a=30
+var b=a--
+console.log(b)
+console.log(a)
+
+//function
+function iphone()
+{
+    console.log("This is my iphone 15")
+}
+iphone()
+
+//function exercise
+var a=20
+var b=30
+function add()
+{
+    console.log(a+b)
+}
+add()
+
+//function paramters
+function area(l,b)
+{
+    console.log("Area is:"+ l/b)
+}
+area(10,10)
+
+//return function
+function add(a,b)
+{
+return (a+b)
+}
+
+var a=30
+var b=40
+var total=add(30,40)
+console.log(total)
+
+//if else
+var rain=false
+
+if(false)
+{
+    console.log("take an umberalla")
+}
+else{
+    console.log("enjoy the sunshine")
+}
+
+//Logical operators
+console.log(false && true && true)
+console.log(false ||  true || true)
+console.log(!false)
+
+//if exercise
+var color="yellow"
+
+if(color=="red")
+{
+    console.log("Stop")
+}
+
+if(color=="yellow")
+{
+    console.log("Get Ready")
+}
+
+if(color=="green")
+{
+    console.log("Go")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
